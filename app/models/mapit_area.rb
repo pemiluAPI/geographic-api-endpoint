0693555d23602e7a057dcdc554487c6f3bb47de9
Erie <erie.suharko@wgs.co.id>
2014-02-24 16:53:17 +0700

    added primary key

--- app/models/mapit_area.rb
+++ app/models/mapit_area.rb
@@ -1,4 +1,5 @@
 class MapitArea < ActiveRecord::Base
     self.table_name = :mapit_area
+    self.primary_key = :id
     has_many :mapit_geometry, foreign_key: "area_id"
 end
--- app/models/mapit_geometry.rb
+++ app/models/mapit_geometry.rb
@@ -1,4 +1,5 @@
 class MapitGeometry < ActiveRecord::Base
+    self.primary_key = :id
     self.table_name = :mapit_geometry
      belongs_to :mapit_area, foreign_key: "area_id"
 end
