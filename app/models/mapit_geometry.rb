0360a2639a0286fe624ad3524be9fad352a3990f
Erie <erie.suharko@wgs.co.id>
2014-02-27 14:04:04 +0700

    update geographic endpoint display data by lembaga

--- app/models/mapit_geometry.rb
+++ app/models/mapit_geometry.rb
@@ -9,28 +9,32 @@ class MapitGeometry < ActiveRecord::Base
       .select("mapit_geometry.id, mapit_geometry.area_id, mapit_area.name,mapit_area.type_id").order("mapit_geometry.id")
       all_polygon = all_polygon.where("ST_Intersects(polygon,ST_GeometryFromText('POINT(? ?)',?))",
           params[:long].to_f, params[:lat].to_f, 4326) unless params[:lat].nil?
-      all_polygon.each do |polygon|
-        if polygon.type_id == 4
-          field = "dapil"
-          unless params[:lat].nil?                  
-            encode_dapil_url  = URI.encode("#{Rails.configuration.pemilu_api_endpoint}/api/dapil?apiKey=#{Rails.configuration.pemilu_api_key}&nama=#{polygon.name}")
-            dapil_end = HTTParty.get(encode_dapil_url, timeout: 500)
-            dapil = dapil_end.parsed_response['data']['results']['dapil'].first
-            caleg_end = HTTParty.get("#{Rails.configuration.pemilu_api_endpoint}/api/caleg?apiKey=#{Rails.configuration.pemilu_api_key}&dapil=#{dapil["id"]}&lembaga=#{params[:lembaga]}", timeout: 500)
-            caleg = caleg_end.parsed_response['data']['results']['caleg'] 
-            polygons << {
-              id_polygon: polygon.id,
-              "#{field}" => polygon.name,
-              caleg: caleg
-            }
+      if params[:lembaga] == "DPR"
+        all_polygon = all_polygon.where("mapit_area.type_id = ?",4)
+        all_polygon.each do |polygon|
+          field = "dapil DPR"
+          unless params[:lat].nil?
+          encode_dapil_url  = URI.encode("#{Rails.configuration.pemilu_api_endpoint}/api/dapil?apiKey=#{Rails.configuration.pemilu_api_key}&nama=#{polygon.name}")
+          dapil_end = HTTParty.get(encode_dapil_url, timeout: 500)
+          dapil = dapil_end.parsed_response['data']['results']['dapil'].first
+          caleg_end = HTTParty.get("#{Rails.configuration.pemilu_api_endpoint}/api/caleg?apiKey=#{Rails.configuration.pemilu_api_key}&dapil=#{dapil["id"]}&lembaga=#{params[:lembaga]}", timeout: 500)
+          caleg = caleg_end.parsed_response['data']['results']['caleg']          
+          polygons << {
+            id_polygon: polygon.id,
+            "#{field}" => polygon.name,
+            caleg: caleg
+          }
           else
             polygons << {
               id_polygon: polygon.id,
               "#{field}" => polygon.name
             }
           end
-        elsif polygon.type_id == 5
-          field = "provinsi"
+        end
+      elsif params[:lembaga] == "DPD"
+        all_polygon = all_polygon.where("mapit_area.type_id = ?",5)
+        all_polygon.each do |polygon|
+          field = "provinsi DPD"
           unless params[:lat].nil? 
             encode_provinsi_url  = URI.encode("#{Rails.configuration.pemilu_api_endpoint}/api/provinsi?apiKey=#{Rails.configuration.pemilu_api_key}&nama=#{polygon.name}")
             provinsi_end = HTTParty.get(encode_provinsi_url, timeout: 500)
@@ -47,6 +51,89 @@ class MapitGeometry < ActiveRecord::Base
               id_polygon: polygon.id,
               "#{field}" => polygon.name
             }
+          end
+        end
+      elsif params[:lembaga] == "DPRDI"
+        all_polygon = all_polygon.where("mapit_area.type_id = ?",6)
+        all_polygon.each do |polygon|
+          field = "dapil DPRDI"
+          unless params[:lat].nil?                  
+          encode_dapil_url  = URI.encode("#{Rails.configuration.pemilu_api_endpoint}/api/dapil?apiKey=#{Rails.configuration.pemilu_api_key}&nama=#{polygon.name}")
+          dapil_end = HTTParty.get(encode_dapil_url, timeout: 500)
+          dapil = dapil_end.parsed_response['data']['results']['dapil'].first
+          caleg_end = HTTParty.get("#{Rails.configuration.pemilu_api_endpoint}/api/caleg?apiKey=#{Rails.configuration.pemilu_api_key}&dapil=#{dapil["id"]}&lembaga=#{params[:lembaga]}", timeout: 500)          
+          caleg = caleg_end.parsed_response['data']['results']['caleg']          
+          polygons << {
+            id_polygon: polygon.id,
+            "#{field}" => polygon.name,
+            caleg: caleg
+          }
+          else
+            polygons << {
+              id_polygon: polygon.id,
+              "#{field}" => polygon.name
+            }
+          end
+        end
+      else
+        all_polygon.each do |polygon|
+          if polygon.type_id == 4
+            field = "dapil DPR"
+            unless params[:lat].nil?                  
+              encode_dapil_url  = URI.encode("#{Rails.configuration.pemilu_api_endpoint}/api/dapil?apiKey=#{Rails.configuration.pemilu_api_key}&nama=#{polygon.name}")
+              dapil_end = HTTParty.get(encode_dapil_url, timeout: 500)
+              dapil = dapil_end.parsed_response['data']['results']['dapil'].first
+              caleg_end = HTTParty.get("#{Rails.configuration.pemilu_api_endpoint}/api/caleg?apiKey=#{Rails.configuration.pemilu_api_key}&dapil=#{dapil["id"]}&lembaga=#{params[:lembaga]}", timeout: 500)
+              caleg = caleg_end.parsed_response['data']['results']['caleg'] 
+              polygons << {
+                id_polygon: polygon.id,
+                "#{field}" => polygon.name,
+                caleg: caleg
+              }
+            else
+              polygons << {
+                id_polygon: polygon.id,
+                "#{field}" => polygon.name
+              }
+            end
+          elsif polygon.type_id == 5
+            field = "provinsi DPD"
+            unless params[:lat].nil? 
+              encode_provinsi_url  = URI.encode("#{Rails.configuration.pemilu_api_endpoint}/api/provinsi?apiKey=#{Rails.configuration.pemilu_api_key}&nama=#{polygon.name}")
+              provinsi_end = HTTParty.get(encode_provinsi_url, timeout: 500)
+              provinsi = provinsi_end.parsed_response['data']['results']['provinsi'].first
+              caleg_end = HTTParty.get("#{Rails.configuration.pemilu_api_endpoint}/api/caleg?apiKey=#{Rails.configuration.pemilu_api_key}&provinsi=#{provinsi["id"]}&lembaga=#{params[:lembaga]}", timeout: 500)
+              caleg = caleg_end.parsed_response['data']['results']['caleg']                    
+              polygons << {
+                id_polygon: polygon.id,
+                "#{field}" => polygon.name,
+                caleg: caleg
+              }
+            else                  
+              polygons << {
+                id_polygon: polygon.id,
+                "#{field}" => polygon.name
+              }
+            end
+            elsif polygon.type_id == 6
+            field = "Dapil DPRDI"
+            unless params[:lat].nil? 
+              encode_dapil_url  = URI.encode("#{Rails.configuration.pemilu_api_endpoint}/api/dapil?apiKey=#{Rails.configuration.pemilu_api_key}&nama=#{polygon.name}")
+              dapil_end = HTTParty.get(encode_dapil_url, timeout: 500)
+              dapil = dapil_end.parsed_response['data']['results']['dapil'].first
+              caleg_end = HTTParty.get("#{Rails.configuration.pemilu_api_endpoint}/api/caleg?apiKey=#{Rails.configuration.pemilu_api_key}&dapil=#{dapil["id"]}&lembaga=#{params[:lembaga]}", timeout: 500)
+              caleg = caleg_end.parsed_response['data']['results']['caleg'] 
+              polygons << {
+                id_polygon: polygon.id,
+                "#{field}" => polygon.name,
+                caleg: caleg
+              }
+            else                  
+              polygons << {
+                id_polygon: polygon.id,
+                "#{field}" => polygon.name
+              }
+            end
           end
         end
       end
